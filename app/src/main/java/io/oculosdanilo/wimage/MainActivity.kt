cc3c26a2b62dead11a42b78efed3f008de28131c
oculosdanilo <oculosdanilo@gmail.com>
2024-06-23 18:59:16 -0300

    morri

--- app/src/main/java/io/oculosdanilo/wimage/MainActivity.kt
+++ app/src/main/java/io/oculosdanilo/wimage/MainActivity.kt
@@ -4,44 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import io.oculosdanilo.wimage.ui.theme.WimageTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            WimageTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+  @OptIn(ExperimentalMaterial3Api::class)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    
+    setContent {
+      WimageTheme {
+        Scaffold(
+          topBar = {
+            TopAppBar(
+              title = { Text(text = "Wimage") },
+            )
+          }
+        ) { padding ->
+          Box(modifier = Modifier.padding(padding))
         }
+      }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WimageTheme {
-        Greeting("Android")
-    }
+  }
 }
